fbac93546c0be86881b00639a6942dc9a97f0596
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:45:21 -0600

    Rewriting as Laminas Project package

--- src/Application.php
+++ src/Application.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-migration for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-migration/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-migration for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-migration/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-migration/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Migration;
+namespace Mezzio\Migration;
 
 class Application extends \Symfony\Component\Console\Application
 {
-    public function __construct(string $name = 'expressive-migration', string $version = 'UNKNOWN')
+    public function __construct(string $name = 'mezzio-migration', string $version = 'UNKNOWN')
     {
         parent::__construct($name, $version);
 
